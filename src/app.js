4ecc8df330929d80a9453834f809c0590dd4fda9
Marc Tucker <marcttucker@gmail.com>
2020-10-27 21:50:50 -0400

    fixed res from delete form

--- src/app.js
+++ src/app.js
@@ -1,41 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config');
-const CategoriesRouter = require('./Categories/categories-router');
-const LearningTracksRouter = require('./LearningTracks/learning-tracks-router');
-const Courses = require('./Courses/courses-router');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const CategoriesRouter = require("./Categories/categories-router");
+const LearningTracksRouter = require("./LearningTracks/learning-tracks-router");
+const Courses = require("./Courses/courses-router");
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-	? 'tiny'
-	: 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 //routes
-app.use('/api/categories', CategoriesRouter);
-app.use('/api/learning-tracks', LearningTracksRouter);
-app.use('/api/courses', Courses);
+app.use("/api/categories", CategoriesRouter);
+app.use("/api/learning-tracks", LearningTracksRouter);
+app.use("/api/courses", Courses);
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
 app.use(function errorHandler(error, req, res, next) {
-	let response
-	if(NODE_ENV === 'production') {
-		response = { error: { message: 'server error' } }
-	} else {
-		console.error(error)
-		response = { message: error.message, error }
-	}
-	res.status(500).json(response)
-})
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { message: error.message, error };
+  }
+  console.error(error);
+  res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;
